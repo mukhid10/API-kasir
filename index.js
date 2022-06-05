f958b98fcc4450a23a0cb4f20cc90bb763e0bcaa
mukhid10 <mukhid976@gmail.com>
2022-06-05 13:42:04 +0700

    /

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("welcome api kasir");
+});
 const allRouter = require("./routes/index");
 app.use(allRouter);
 
